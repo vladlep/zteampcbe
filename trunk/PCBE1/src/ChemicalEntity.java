b22a603a49bddf12b7dffb9dc1cbc28aa3e8cf09
vlad.lep <vlad.lep@66b5afc6-b9ad-cf0b-afb5-527758367111>
2010-10-23 09:22:36 +0000

    Initial import.

---
+++ trunk/PCBE1/src/ChemicalEntity.java
@@ -0,0 +1,14 @@
+
+public class ChemicalEntity {
+
+	private int positionOnX;
+	private int positionOnY;
+
+	public ChemicalEntity(int positionOnx, int positionOnY)
+	{
+		this.positionOnX = positionOnx;
+		this.positionOnY = positionOnY;
+	}
+	
+	
+}
---
+++ trunk/PCBE1/src/Molecules.java
@@ -0,0 +1,16 @@
+
+public class Molecules extends ChemicalEntity{
+
+	public Molecules(int positionOnx, int positionOnY)
+	{
+		super(positionOnx, positionOnY);
+		
+	}
+	
+	
+	public String toString() {
+	
+	return "";
+	}
+	
+}
